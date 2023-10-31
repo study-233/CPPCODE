33d5f6c049c543c30c113e633e19f97fc22b72a8
TABLE“git config --global user.name TABLE“ <3152451821@qq.com>
2023-10-31 16:44:20 +0800

    修改注释

--- CODE_Cpp/homework/week 7/8.cpp
+++ CODE_Cpp/homework/week 7/8.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 using namespace std;
-void move(char A, char C,int n){
+void move(char A, char C,int n){ //输出
     cout<<A<<" to "<<C<<endl;
 }
-void Hanno(char A, char B, char C, int n)
+void Hanno(char A, char B, char C, int n) //n表示移动几个圆盘
 {
 	if (n == 1){
-		move(A, C, n);
+		move(A, C, n); //只剩一个时直接直接移动
 	}
 	else{
-		//将n-1个圆盘从A柱借助于C柱移动到B柱上
-		Hanno(A, C, B, n - 1);
-		//将A柱子最后一个圆盘移动到C柱上
-		move(A, C, n);
-		//将n-1个圆盘从B柱借助于A柱移动到C柱上
-		Hanno(B, A, C, n - 1);
+		Hanno(A, C, B, n - 1); //将n-1个圆盘从A柱借助于C柱移动到B柱上
+		move(A, C, n); //将A柱子最后一个圆盘移动到C柱上
+		Hanno(B, A, C, n - 1); //将n-1个圆盘从B柱借助于A柱移动到C柱上
 	}
 }
 int main(){
     int n;
     cin>>n;
     Hanno('A','B','C',n);
+	system("");
 }
