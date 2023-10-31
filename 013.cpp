f7f1acba1e497cbd821bf72f5971a10100520264
TABLE“git config --global user.name TABLE“ <3152451821@qq.com>
2023-10-31 19:30:28 +0800

    mooc提交

---
+++ 013.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    double a,b,c,d,x1,x2,jp,ip;
+    scanf("%lf%lf%lf",&a,&b,&c);
+    d=b*b-4*a*c;
+    if(d==0){  
+        x1 = -b/(2*a);
+        printf("x1=x2=%.5f",x1);
+    }
+    if(d>0){
+        x1 = (-b + sqrt(d))/(2*a);
+        x2 = (-b - sqrt(d))/(2*a);
+        printf("x1=%.5f;x2=%.5f",x1,x2);
+    }
+    if(d<0){
+        jp = -b/(2*a);
+        ip = sqrt(-d)/(2*a);
+        printf("x1=%.5f+%.5fi;",jp,ip);
+        printf("x1=%.5f-%.5fi",jp,ip);
+    }
+
+}
---
+++ CODE_Cpp/homework/week 5/9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    double a,b,c,d,x1,x2,jp,ip;
+    cin>>a>>b>>c;
+    d=b*b-4*a*c;
+    if(d==0){  
+        x1 = -b/(2*a);
+        cout<<"has two equal roots:"<<endl<<"x1=x2="<<x1;
+    }
+    if(d>0){
+        x1 = (-b + sqrt(d))/(2*a);
+        x2 = (-b - sqrt(d))/(2*a);
+        cout<<"has two real roots:"<<endl<<"x1="<<x1<<","<<"x2="<<x2;
+    }
+    if(d<0){
+        jp = -b/(2*a);
+        ip = sqrt(-d)/(2*a);
+        cout<<"has two complex roots:"<<endl;
+        cout<<"x1="<<jp<<"+"<<ip<<"i"<<endl;
+        cout<<"x2="<<jp<<"-"<<ip<<"i";
+    }
+
+}
