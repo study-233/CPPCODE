fddb1f02fa4cb5361011a7235f5538bbd4fc4c00
mrtao <3152451821@qq.com>
2023-11-09 22:26:45 +0800

    mooc 8

--- MOOC/035.cpp
+++ MOOC/035.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 //用递归 求Pell数列第k项
 int Pell(int k){
-    int temp=1;
-    for(;k;k--){
-        temp=(temp*2)%32767;
-    }
-    return temp;
+    if(a[k]!=0) return a[k];
+    a[k]=(2*Pell(k-1)+Pell(k-2))%32767;
+    return a[k];
 }
 int main(){
-    freopen("D:\\cppcode\\cppcode\\MOOC\\in.txt","r",stdin);
+    //freopen("D:\\cppcode\\cppcode\\MOOC\\in.txt","r",stdin);
     int n,x;
     cin>>n;
     for(int i=0;i<n;i++){
