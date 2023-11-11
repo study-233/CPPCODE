e9c678df460db7305114427fb8c1bdb69ff69d04
mrtao <3152451821@qq.com>
2023-11-11 12:11:54 +0800

    MOOC 8

--- MOOC/038.cpp
+++ MOOC/038.cpp
@@ -3,17 +3,17 @@
 #include <stdio.h>
 using namespace std;
 
-int bitManipulation1(int n, int i) {
-return ((~(1<<i))&n)^n;
+int bitManipulation2(int n, int i) {
+return ((1<<i)^n);
 }
 
 int main() {
-    freopen("D:\\cppcode\\cppcode\\MOOC\\in.txt","r",stdin);
+    //freopen("D:\\cppcode\\cppcode\\MOOC\\in.txt","r",stdin);
 	int n, i, t;
 	cin >> t;
 	while (t--) { 
 		cin >> n >> i;
-		cout << bitManipulation1(n, i) << endl;
+		cout << bitManipulation2(n, i) << endl;
 	}
 	return 0;
 }
---
+++ MOOC/039.cpp
@@ -0,0 +1,19 @@
+//039:编程填空：左边i位取反
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int bitManipulation3(int n, int i) {
+return ((1<<i)^n);
+}
+
+int main() {
+    freopen("E:\\cppcode\\cppcode\\MOOC\\in.txt","r",stdin);
+	int n, i, t;
+	cin >> t;
+	while (t--) { 
+		cin >> n >> i;
+		cout << bitManipulation3(n, i) << endl;
+	}
+	return 0;
+}
