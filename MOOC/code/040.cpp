346e1233c0c9432db0c1966fb766490ba483e6f7
mrtao <3152451821@qq.com>
2023-11-12 17:24:04 +0800

    040

--- MOOC/code/040.cpp
+++ MOOC/code/040.cpp
@@ -2,9 +2,6 @@
 #include <stdio.h>
 using namespace std;
 int main(){
-    //COMPUTER
-    //freopen("E:\\Codefield\\cppcode\\MOOC\\code\\in.txt","r",stdin);
-    //TABLE
-    //freopen("D:\\cppcode\\cppcode\\MOOC\\code\\in.txt","r",stdin);
+    //freopen("E:\\CPPCODE\\MOOC\\code\\in.txt","r",stdin);
     
 }
--- MOOC/code/test.cpp
+++ MOOC/code/test.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main(){
     //freopen("E:\\CPPCODE\\MOOC\\code\\in.txt","r",stdin);
 
-    //test
 }
 
 
