932e01dbd3a1e41f385ef6bdcad69cffe3cb0a71
mrtao <3152451821@qq.com>
2023-11-12 17:21:43 +0800

    修改

--- MOOC/code/test.cpp
+++ MOOC/code/test.cpp
@@ -2,11 +2,8 @@
 #include <stdio.h>
 using namespace std;
 int main(){
-    //COMPUTER
-    //freopen("E:\\Codefield\\cppcode\\MOOC\\code\\in.txt","r",stdin);
-    //TABLE
-    //freopen("D:\\cppcode\\cppcode\\MOOC\\code\\in.txt","r",stdin);
-    //test
+    //freopen("E:\\CPPCODE\\MOOC\\code\\in.txt","r",stdin);
+
 }
 
 
