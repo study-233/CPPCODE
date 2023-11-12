d166689925f0026a09f233990ff9cc294b5e65be
mrtao <3152451821@qq.com>
2023-11-12 17:01:49 +0800

    test

--- MOOC/code/test.cpp
+++ MOOC/code/test.cpp
@@ -6,7 +6,7 @@ int main(){
     //freopen("E:\\Codefield\\cppcode\\MOOC\\code\\in.txt","r",stdin);
     //TABLE
     //freopen("D:\\cppcode\\cppcode\\MOOC\\code\\in.txt","r",stdin);
-
+    //test
 }
 
 
