572b3a7f77c3405199e8d371139cedff2b0b1f21
mrtao <3152451821@qq.com>
2023-11-27 22:08:01 +0800

    mooc

--- MOOC/code/054.cpp
+++ MOOC/code/054.cpp
@@ -1,15 +1,34 @@
 /* 054:成绩排序 */
-#include <iostream>
-#include <stdio.h>
-#include <cstring>
+#include <bits/stdc++.h>
+struct Student{
+    char name[20];
+    int score;
+};
 using namespace std;
 int main(){
     //freopen("E:\\CPPCODE\\MOOC\\code\\in.txt","r",stdin);
     int n;
     cin>>n;
-    struct student[n];
-    
-
-    
+    Student cla[n];
+    for(int i=0;i<n;i++){
+        cin>>cla[i].name;
+        cin>>cla[i].score;
+    }
+    for(int i=0;i<n-1;i++){
+        int p=i;
+        for(int j=i+1;j<n;j++){
+            if(cla[p].score<cla[j].score)
+            p=j;
+            else if(cla[p].score==cla[j].score && strcmp(cla[p].name,cla[j].name)>0)
+            p=j;
+        }
+        Student temp = cla[p];
+        cla[p] = cla[i];
+        cla[i] = temp;
+    }
+    for(int i=0;i<n;i++){
+        cout<<cla[i].name<<" ";
+        cout<<cla[i].score<<endl;
+    }
 }
 
--- MOOC/code/055.cpp
+++ MOOC/code/055.cpp
@@ -1,8 +1,7 @@
-/**/
+/* 055:分数线划定 */
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
     //freopen("E:\\CPPCODE\\MOOC\\code\\in.txt","r",stdin);
 
 }
-
