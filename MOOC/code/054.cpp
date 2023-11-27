84f0b09880ba4af32c00640f551bba5bf93c964e
TABLE“git config --global user.name TABLE“ <3152451821@qq.com>
2023-11-27 18:47:13 +0800

    mooc

---
+++ MOOC/code/054.cpp
@@ -0,0 +1,15 @@
+/* 054:成绩排序 */
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+using namespace std;
+int main(){
+    //freopen("E:\\CPPCODE\\MOOC\\code\\in.txt","r",stdin);
+    int n;
+    cin>>n;
+    struct student[n];
+    
+
+    
+}
+
