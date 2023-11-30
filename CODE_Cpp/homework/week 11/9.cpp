97b854d1acaafc5b2699f041480d9c7c58efc889
TABLE“git config --global user.name TABLE“ <3152451821@qq.com>
2023-11-30 20:22:13 +0800

    week 11

--- CODE_Cpp/homework/week 11/9.cpp
+++ CODE_Cpp/homework/week 11/9.cpp
@@ -8,37 +8,32 @@ int main(){
     int a[n][n];
     int row = 0;
     int col = n-1;
-    int left=0,up=0;
-    int right=n-1,down=n-1;
-    for(int i=1;i<=n*n;i++){
-        a[row][col]=i;
-
-        if(col==right){
-            if(row==down) {col--;}
-            else row++;
-        }
-        else if(col==left){
-            if(row==up) {col++;}
-            else row--;
-        }        
-        else if(row==up){
-            if(col==right) {row++;}
-            else col++;
-        }
-        else if(row==down){
-            if(col==left) {row--;}
-            else col--;
-        }
-
+    int left=0,top=0;
+    int right=n-1,bottom=n-1;
+    int i=1;
+    while(i<=n*n){
+        row=top;
+        while(row<=bottom){
+            a[row++][right]=i++;}
+        right--;
+        col=right;
+        while(col>=left){
+            a[bottom][col--]=i++;}
+        bottom--;        
+        row=bottom;
+        while(row>=top){
+            a[row--][left]=i++;}
+        left++;
+        col=left;
+        while(col<=right){
+            a[top][col++]=i++;}
+        top++;
     }
-
-
-
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            cout<<a[i][j]<<" ";
+            printf("%4d ",a[i][j]);
         }
-        cout<<endl;
+        printf("\n");
     }
 }
 
