ec930e64c9c3944d2fe32e7d01bf9818c5e89191
mrtao <3152451821@qq.com>
2023-12-03 18:41:45 +0800

    mooc

--- MOOC/part 1/code/056.cpp
+++ MOOC/part 1/code/056.cpp
@@ -2,30 +2,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 struct inf{
-    int ID;
+    char ID[10];
     int age;
+    int order;
 };
 int main(){
-    freopen("E:\\CPPCODE\\MOOC\\in.txt","r",stdin);
+    //freopen("E:\\CPPCODE\\MOOC\\in.txt","r",stdin);
     int num;
     cin>>num;
     inf patient[num];
     for(int i=0;i<num;i++){
         cin>>patient[i].ID>>patient[i].age;
+        patient[i].order=i;
     }
     for(int i=0;i<num-1;i++){
         int p=i;
         for(int j=i+1;j<num;j++){
-            if(patient[p].age<60 && patient[j].age<60){
-                if(patient[p].ID>patient[j].ID) p=j;
-            }
-            else if(patient[p].age>=60 && patient[j].age>=60){
-                if(patient[p].age>patient[j].age) p=j;
-                else if(patient[p].age==patient[j].age){
-                    if(patient[p].ID>patient[j].ID) p=j;
+            if(patient[p].age>=60 && patient[j].age>=60){
+                if(patient[p].age < patient[j].age) p=j;
+                else if(patient[p].age == patient[j].age){
+                    if(patient[p].order > patient[j].order) p=j;
                 }
+            }
+            else if(patient[p].age<60 && patient[j].age<60){
+                if(patient[p].order > patient[j].order) p=j;
+            }
             else{
-                if(patient[p].age>patient[j].age) p=j;
+                if(patient[j].age>=60) p=j;
             }
         }
         inf temp = patient[p];
