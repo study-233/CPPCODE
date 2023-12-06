7f06fdb8630939a921afc5895ccf48ccf56dad51
TABLE“git config --global user.name TABLE“ <3152451821@qq.com>
2023-12-06 14:08:28 +0800

    week 11

--- CODE_Cpp/homework/week 11/part/2.cpp
+++ CODE_Cpp/homework/week 11/part/2.cpp
@@ -5,22 +5,30 @@ struct  Person  {
     long  id;
     float  salary;
 };
-Person  allone[6]={{"jone",12345,339.0},{"david",13916,449.0},{"marit",27519,311.0},
-{"jasen",42876,623.0},{"peter",23987,400.0},{"yoke",12335,511.0}};
+
+Person  allone[6]={{"jone",12345,339.0},{"david",13916,449.0},{"marit",27519,311.0},{"jasen",42876,623.0},{"peter",23987,400.0},{"yoke",12335,511.0}};
+
 int  main()
 {
     Person  *temp;
     int  i,j;
     Person*  index[6];
-    for(int  p1=0;p1<5;p1++){
-        int p = p1;
-        for (int  j=p+1;j<6;j++){
-            if(index[p]->salary > index[j]->salary) p=j;
-        }
-        temp = index[p];
-        index[p] = index[j];
-        index[j] = temp;
+    //初始化索引数列
+    for(i=0;i<6;i++){
+        index[i] = allone+i;
     }
+    //排序
+    for(i=1;i<6;i++){
+        for (j=0;j<6-i;j++){
+            if(index[j]->salary > index[j+1]->salary){
+                temp = index[j];
+                index[j] = index[j+1];
+                index[j+1] = temp;
+            }
+        }
+    }
+
+
     for(int  k=0;k<6;k++){
         cout  <<  index[k]->name  <<  "  "<<  index[k]->id  <<  "  "  <<  index[k]->salary  <<  endl;        
     }
