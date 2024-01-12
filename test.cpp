94d77af8e269c3a74c55153cbfb2bead3c9edf6b
mrtao <3152451821@qq.com>
2024-01-12 11:08:29 +0800

    completion

--- test.cpp
+++ test.cpp
@@ -2,9 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main(){
     //freopen("E:\\CPPCODE\\in.txt","r",stdin);
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);cout.tie(nullptr);
-
+    struct point
+    {
+        int x;
+        int y;
+    }p;
+    p={1,2};
+    cout<<p.x;
 }
