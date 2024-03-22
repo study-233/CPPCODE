58f52f73df0a188f06877afe0fd2605d6e847a5c
mrtao <3152451821@qq.com>
2024-03-22 23:17:52 +0800

    exercise 5

---
+++ CODE_Cpp/2023 program design homework/exercise5/1.cpp
@@ -0,0 +1,19 @@
+/**/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+const ll N = 2e5 + 9;
+
+int main(){
+    //freopen("E:\CPPCODE\in.txt","r",stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+    
+    int x;
+    cin>>x;
+    if(x<3) cout<<"A";
+    else cout<<"B";
+
+    return 0;
+}
