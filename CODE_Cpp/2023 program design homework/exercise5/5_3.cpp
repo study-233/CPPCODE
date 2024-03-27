5eec47d6054361d626f3a8e2089cdc8a02a14e1f
mrtao <3152451821@qq.com>
2024-03-27 23:31:33 +0800

    exercise5

--- CODE_Cpp/2023 program design homework/exercise5/5_3.cpp
+++ CODE_Cpp/2023 program design homework/exercise5/5_3.cpp
@@ -1,41 +1,27 @@
-/**/
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define endl '\n'
-const ll N = 2e5 + 9;
-int fruit[100];
+int main()
+{
+	int t;
+    int n,m,line=100,ans=0;
 
-int main(){
-    //freopen("E:\CPPCODE\in.txt","r",stdin);
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);cout.tie(nullptr);
-    
-    int T;
-    int banana_num,tools_num;
-    int ans=0,line=100;
-    cin>>T;
-    while(T--){
-        cin>>banana_num>>tools_num;
-        int bs[n+1];
-        for(int i=1;i<=banana_num;i++) cin>>bs[i];
-        if(tools_num >= banana_num){
-            cout<<100<<endl;
-            continue;
-        }
-        bs[0]=0;
-        bs[banana_num+1]=line+1;
-		int r=tools_num;
-		line=1;//长度变成1 
-		while(r<=banana_num)//控制m<=n
-		{
-			ans=max(ans,mp[r+1]-mp[line-1]+1-2);//因为要找最大的,所以用Max函数 
-			line++;
+	cin>>t;
+	while(t--){
+		cin>>n>>m;
+		int mp[n+1];
+		for(int i=1;i<=n;i++) cin>>mp[i];
+		if(m>=n){
+			cout<<100<<endl;
+			continue;
+		}
+		mp[0]=0;
+		mp[n+1]=line+1;
+		int r=m;
+		while(r<=n){         
+			ans=max(ans,mp[r+1]-mp[r-m]-1);    //因为要找最大的,所以用Max函数 
 			r++;
 		}
 		cout<<ans<<endl; 
-    }
-
-
-    return 0;
+	}
+	return 0;
 }
